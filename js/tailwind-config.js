e88722daa1058905a422837e79120fbbb91a8bb7
doanhxd <doanhmiu99@gmail.com>
2023-01-31 23:53:22 +0700

    update page with :heart:

---
+++ js/tailwind-config.js
@@ -0,0 +1,33 @@
+tailwind.config = {
+  theme: {
+    container: {
+      center: true,
+    },
+    extend: {
+      colors: {
+        pinky: '#FFF1F3',
+        t_pri: '#000',
+        t_pri_dark: '#fff',
+        t_sec: '#3f3f46',
+        t_sec_dark: '#e4e4e7',
+        card: '#fafafa',
+        card_dark: '#18181b',
+        page: '#fff',
+        page_dark: '#000',
+        nav_dark: '#000',
+        btn: '#000',
+        btn_dark: '#fff'
+      }
+    }
+  },
+}
+//     --color-text-primary: #000;
+//     --color-text-primary-dark: #fff;
+//     --color-text-secondary: #3f3f46;
+//     --color-text-secondary-dark: #e4e4e7;
+//     --color-card: #fafafa;
+//     --color-card-dark: #18181b;
+//     --color-page: #fff;
+//     --color-page-dark: #000;
+//     --color-button: #000;
+//     --color-button-dark: #fff;
